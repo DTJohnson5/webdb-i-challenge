9f676bf87467ffd62c5e7c0f8ffd356d2b9b30c1
David.Johnson <davidt.johnson75@yahoo.com>
2019-11-04 16:37:46 -0600

    Able to get all accounts

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/api", (req, res) => {
+  db("accounts")
+    .then(accts => res.status(200).json(accounts))
+
+    .catch(error =>
+      res.status(500).json({
+        Error: "Your attempt to retrieve the accounts have failed."
+      })
+    );
+});
 
 module.exports = server;
